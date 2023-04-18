2e5178d5cb96c92cc2457b82a2fda4545a171f50
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-18 23:51:10 +0530

    loaded dataset

--- README.md
+++ README.md
@@ -1,15 +1,27 @@
 # Parameter Optimization of SVM | Assignment 5
-## Using  Chess (King-Rook vs. King) Data Set 
-### 1. Number of Instances: 28056
 
-### 2. Number of Attributes: There are six attribute variables and one class variable.
+## Dry Bean Dataset
+
+### 1. Number of Instances: 13611
+
+### 2. Number of Attributes: 17
 
 ### 3. Attribute Information:<br>
-   1. White King file (column)<br>
-   2. White King rank (row)<br>
-   3. White Rook file<br>
-   4. White Rook rank<br>
-   5. Black King file<br>
-   6. Black King rank<br>
-   7. optimal depth-of-win for White in 0 to 16 moves, otherwise drawn
-	{draw, zero, one, two, ..., sixteen}.<br>
+
+1.) Area (A): The area of a bean zone and the number of pixels within its boundaries.<br>
+2.) Perimeter (P): Bean circumference is defined as the length of its border.<br>
+3.) Major axis length (L): The distance between the ends of the longest line that can be drawn from a bean.<br>
+4.) Minor axis length (l): The longest line that can be drawn from the bean while standing perpendicular to the main axis.<br>
+5.) Aspect ratio (K): Defines the relationship between L and l.<br>
+6.) Eccentricity (Ec): Eccentricity of the ellipse having the same moments as the region.<br>
+7.) Convex area (C): Number of pixels in the smallest convex polygon that can contain the area of a bean seed.<br>
+8.) Equivalent diameter (Ed): The diameter of a circle having the same area as a bean seed area.<br>
+9.) Extent (Ex): The ratio of the pixels in the bounding box to the bean area.<br>
+10.)Solidity (S): Also known as convexity. The ratio of the pixels in the convex shell to those found in beans.<br>
+11.)Roundness (R): Calculated with the following formula: (4piA)/(P^2)<br>
+12.)Compactness (CO): Measures the roundness of an object: Ed/L<br>
+13.)ShapeFactor1 (SF1)<br>
+14.)ShapeFactor2 (SF2)<br>
+15.)ShapeFactor3 (SF3)<br>
+16.)ShapeFactor4 (SF4)<br>
+17.)Class (Seker, Barbunya, Bombay, Cali, Dermosan, Horoz and Sira)<br>
--- main.r
+++ main.r
@@ -1,2 +1,60 @@
-df<-read.csv(url('https://archive.ics.uci.edu/ml/machine-learning-databases/chess/king-rook-vs-king/krkopt.data'))
-df.head()
+#Code by Deepankar Varma(102003431),3CO17
+#Library Downloading and Inclusion
+#install.packages("kernlab")
+#library(kernlab)
+df <- read.csv("C:\\Users\\acer\\Desktop\\Thapar\\Subjects\\6th Semester\\UCS654 PREDICTIVE ANALYTICS USING STATISTICS\\PS Rana Ass\\Parameter Optimization Using SVM\\Dry_Bean_Dataset.csv")
+head(df)
+
+#Variables Declaration
+bestAccuracy=0
+bestKernel=""
+bestNu=0
+bestEpsilon=0
+iteration=100
+kernelList=c('rbfdot','polydot','vanilladot','tanhdot','laplacedot','anovadot')
+# Split the data into 70:30 train-test split
+trainSize <- floor(nrow(df)*0.7)
+trainIndex <- sample(seq_len(nrow(df)), size = trainSize)
+trainDataset <- df[trainIndex,]
+testDataset <- df[-trainIndex,]
+trainDataset
+# Extract the formula for model training
+formula <- as.formula(paste("x11 ~ ", paste(colnames(trainDataset)[2:7], collapse = " + ")))
+
+
+#Fitness Function Definition
+fitnessFunction<-function(k,n,e){
+  #k stands for Kernel,n for Nu, e for Epsilon
+  #Building the model
+  model<-ksvm(formula,trainDataset,kernel=k,nu=n,epsilon=e,kpar=list())
+  #Prediction of Testing Dataset
+  predicted<-round(predict(model,testDataset))
+  #Model Evaluation Accuracy
+  accuracy<-round(mean(actual==predicted)*100,2)
+  return(accuracy)  
+}
+# Initialize empty vectors to store iteration numbers and accuracies
+iter_vec <- c()
+accuracy_vec <- c()
+
+# Run the iterations
+for(i in 1:iteration){
+  k <- sample(kernelList, 1)
+  n <- runif(1)
+  e <- runif(1)
+  Accuracy <- fitnessFunction(k, n, e)
+  if(Accuracy > bestAccuracy){
+    bestKernel <- k
+    bestNu <- n
+    bestEpsilon <- e
+    bestAccuracy <- Accuracy
+  }
+  # Append iteration number and accuracy to the vectors
+  iter_vec <- c(iter_vec, i)
+  accuracy_vec <- c(accuracy_vec, bestAccuracy)
+  
+  
+}
+
+# Plot the convergence graph
+plot(iter_vec, accuracy_vec, type = "l", xlab = "Iteration", ylab = "Accuracy")

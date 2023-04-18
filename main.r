12dad188532662af05339754c6159daabd490ff2
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-19 00:35:39 +0530

    updated code

--- main.r
+++ main.r
@@ -1,9 +1,14 @@
 #Code by Deepankar Varma(102003431),3CO17
 #Library Downloading and Inclusion
 #install.packages("kernlab")
-#library(kernlab)
+library(kernlab)
 df <- read.csv("C:\\Users\\acer\\Desktop\\Thapar\\Subjects\\6th Semester\\UCS654 PREDICTIVE ANALYTICS USING STATISTICS\\PS Rana Ass\\Parameter Optimization Using SVM\\Dry_Bean_Dataset.csv")
-head(df)
+# Convert the Class column to a factor
+df$Class <- factor(df$Class, levels = c("SEKER", "BARBUNYA", "BOMBAY", "CALI","DERMASON","HOROZ","SIRA"))
+
+# Map the factor levels to numeric values
+df$Class <- as.numeric(df$Class)
+df$Class
 
 #Variables Declaration
 bestAccuracy=0
@@ -17,27 +22,28 @@ trainSize <- floor(nrow(df)*0.7)
 trainIndex <- sample(seq_len(nrow(df)), size = trainSize)
 trainDataset <- df[trainIndex,]
 testDataset <- df[-trainIndex,]
-trainDataset
+ncol(trainDataset)
+nrow(trainDataset)
 # Extract the formula for model training
-formula <- as.formula(paste("x11 ~ ", paste(colnames(trainDataset)[2:7], collapse = " + ")))
-
+X <- as.matrix(trainDataset[, 1:16])
+Y <- as.factor(trainDataset$Class)
 
 #Fitness Function Definition
 fitnessFunction<-function(k,n,e){
   #k stands for Kernel,n for Nu, e for Epsilon
   #Building the model
-  model<-ksvm(formula,trainDataset,kernel=k,nu=n,epsilon=e,kpar=list())
+  model<-ksvm(X, Y, kernel=k, nu=n, epsilon=e,kpar=list())
   #Prediction of Testing Dataset
-  predicted<-round(predict(model,testDataset))
+  predicted<-predict(model,testDataset[, -1])
   #Model Evaluation Accuracy
-  accuracy<-round(mean(actual==predicted)*100,2)
+  accuracy<-round(mean(as.numeric(testDataset$Class==predicted))*100,2)
   return(accuracy)  
 }
 # Initialize empty vectors to store iteration numbers and accuracies
 iter_vec <- c()
 accuracy_vec <- c()
 
-# Run the iterations
+#Run the iterations
 for(i in 1:iteration){
   k <- sample(kernelList, 1)
   n <- runif(1)
@@ -52,8 +58,8 @@ for(i in 1:iteration){
   # Append iteration number and accuracy to the vectors
   iter_vec <- c(iter_vec, i)
   accuracy_vec <- c(accuracy_vec, bestAccuracy)
-  
-  
+
+
 }
 
 # Plot the convergence graph
